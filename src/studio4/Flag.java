d100afc1ac55d2447a912d7e304f4367b29e877f
xiao- <xiao-@MHarker.dhcp.wustl.edu>
2022-10-13 14:08:49 -0500

    Flag credit to McDonald

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,21 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+
+		StdDraw.setPenColor(255,0,0);
+		StdDraw.filledRectangle(0.5, 0.5, 1, 1);
+		
+		StdDraw.setPenColor(255,234,0);
+		StdDraw.filledEllipse(0.4, 0.4, 0.1, 0.3);
+		StdDraw.filledEllipse(0.57, 0.4, 0.1, 0.3);
+		
+		StdDraw.setPenColor(255,0,0);
+		StdDraw.filledEllipse(0.4, 0.4, 0.05, 0.22);
+		StdDraw.filledEllipse(0.57, 0.4, 0.05, 0.22);
+		
+		StdDraw.setPenColor(255,0,0);
+		StdDraw.filledRectangle(0, 0, 1, 0.4);
+		
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,43 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String name = in.next();
+		int redcomp = in.nextInt();
+		int greencomp = in.nextInt();
+		int bluecomp = in.nextInt();
+		boolean isfilled = in.nextBoolean();
+		
+		double p1 = in.nextDouble();
+		double p2 = in.nextDouble();
+		double p3 = in.nextDouble();
+		double p4 = in.nextDouble();
+		StdDraw.setPenColor(redcomp,greencomp,bluecomp);
+		
+		
+		if (name.equals("triangle")) {
+		double p5 = in.nextDouble();
+		double p6 = in.nextDouble();
+		double[] x = {p1,p3,p5};
+		double[] y = {p2,p4,p6};
+		StdDraw.polygon(x,y);
+			if (isfilled==true) {
+				StdDraw.filledPolygon(x,y);
+			}
+		}
+	
+		if (name.equals("rectangle")) {
+		StdDraw.rectangle(p1,p2,p3,p4);
+			if (isfilled==true) {
+				StdDraw.filledRectangle(p1,p2,p3,p4);
+			}
+		}
+		
+		if (name.equals("ellipse")) {
+		StdDraw.ellipse(p1, p2, p3, p4);
+			if (isfilled==true) {
+				StdDraw.filledEllipse(p1, p2, p3, p4);
+			}
+		}
+		
 	}
 }
